a9664d903d201ed41b5e4323814f0a5993fbe6bf
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2020-10-17 12:12:55 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/SFML-CLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFML-CLion.dir/main.cpp.obj"
+  "SFML-CLion.exe"
+  "SFML-CLion.exe.manifest"
+  "SFML-CLion.lib"
+  "SFML-CLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFML-CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SFML_CLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFML_CLion.dir/main.cpp.obj"
+  "SFML_CLion.exe"
+  "SFML_CLion.exe.manifest"
+  "SFML_CLion.lib"
+  "SFML_CLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFML_CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
